5e35e98c85071780b371c6e44338e3e4b0719dde
shanti-gzmn <shantiguzmag@gmail.com>
2023-05-06 17:08:45 -0600

    Se agrega metodo GET y diccionario con peliculas

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version = '0.0.1'
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }, 
+        {
+        'id': 2,
+        'title': 'Avatar 2',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }  
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1> Hello world! </h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id:int):
+    for item in movies:
+        if item['id'] == id:
+            return item
+    return []
+    

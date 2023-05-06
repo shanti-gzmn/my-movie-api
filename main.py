558856f62409485d47ba380a03b5af6b33591e56
shanti-gzmn <shantiguzmag@gmail.com>
2023-05-06 13:16:16 -0600

    El primer commit del proyecto con FastAPI

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.title = "Mi aplicacion con FastAPI"
+app.version = '0.0.1'
+
+@app.get('/', tags=['home'])
+def message():
+    return "Hello world!"

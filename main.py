cc23fa2b70525d5dba563fef1b4127af3cd8ed13
shanti-gzmn <shantiguzmag@gmail.com>
2023-05-07 16:00:13 -0600

    Validation using Field from pydantic added

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+
 
 app = FastAPI()
 app.title = "My app with FastAPI"
 app.version = '0.0.1'
 
+
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview : str
-    year: int
+    title: str = Field(min_length = 5, max_length=15)
+    overview : str = Field(min_length=15, max_length=50)
+    year: int = Field(le = 2022)
     rating: float
     category: str
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'id': 1,
+                'title': "My movie",
+                'overview': "Movie's description",
+                'year': 2022, 
+                'rating': 9.8,
+                'category': 'Accion'
+            }
+        }
+
 
 movies = [
     {

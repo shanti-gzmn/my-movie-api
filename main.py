9a15eda00737e7c0cf2e46d379654de0d1940f56
shanti-gzmn <shantiguzmag@gmail.com>
2023-05-07 14:16:58 -0600

    CRUD Module complete

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.title = "Mi aplicacion con FastAPI"
+app.title = "My app with FastAPI"
 app.version = '0.0.1'
 
 movies = [
@@ -18,7 +18,7 @@ movies = [
         'id': 2,
         'title': 'Avatar 2',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
+        'year': '2022',
         'rating': 7.8,
         'category': 'Acción'    
     }  
@@ -40,3 +40,42 @@ def get_movie(id:int):
             return item
     return []
     
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category:str, year:str):
+    return [item for item in movies if item['category'] == category]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        'id': id,
+        'title': title,
+        'overview': overview, 
+        'year': year, 
+        'category': category
+    })
+    return movies 
+
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = title
+            item['overview'] = overview
+            item['year'] = year
+            item['rating'] = rating 
+            item['category'] = category
+            return movies
+
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item['id'] == id:
+            movies.remove(item)
+            return movies
+
+
+
+# uvicorn main:app --reload 
+# uvicorn main:'Name_of_instance' --reload 

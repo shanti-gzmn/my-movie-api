a93eddbdcb6eb5141c2d47b4b0a6c8f8d223920a
shanti-gzmn <shantiguzmag@gmail.com>
2023-05-30 20:35:25 -0600

    App terminada

--- routers/movie.py
+++ routers/movie.py
@@ -51,10 +51,9 @@ def update_movie(id: int, movie: Movie) -> dict:
 @movie_router.delete('/movies/{id}', tags=['movies'], response_model = dict, status_code = 200)
 def delete_movie(id: int) -> dict:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if not result:
         return JSONResponse(status_code=404, content={'message': 'No encontrado'})
-    db.delete(result)
-    db.commit()
+    MovieService(db).delete_movie(id)
     return JSONResponse(status_code = 200, content = {'message': "Movie successfully deleted"})
 
